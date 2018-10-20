bca64ada91791dc859e17c93deca0ee562cc0c82
guodidi <gyh657614602@126.com>
2018-10-21 04:21:44 +0800

    修改配置顺序

--- multi-same-type-datasource/src/main/java/com/github/guoyaohui/MasterSlaveServer.java
+++ multi-same-type-datasource/src/main/java/com/github/guoyaohui/MasterSlaveServer.java
@@ -3,7 +3,6 @@ package com.github.guoyaohui;
 import com.github.guoyaohui.annotation.EnableMybatisInteceptor;
 import com.github.guoyaohui.mybatis.interceptor.ShowFullSqlInterceptor;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -13,11 +12,10 @@ import org.springframework.context.annotation.Import;
  * @author 郭垚辉
  * @date 2018/10/17
  */
-@EnableMybatisInteceptor
-@SpringBootApplication
 @EnableAspectJAutoProxy
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableMybatisInteceptor
 @Import(ShowFullSqlInterceptor.class)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MasterSlaveServer {
 
     public static void main(String[] args) {
